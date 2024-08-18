394e11ccd6966695d85dd1ba82314219594d3efa
Stanislav <mail@skov.dev>
2024-08-18 15:26:16 +0300

    Launched service-discovery

---
+++ service-discovery/src/main/java/local/pms/servicediscovery/SpringBootRunServiceDiscovery.java
@@ -0,0 +1,15 @@
+package local.pms.servicediscovery;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SpringBootRunServiceDiscovery {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRunServiceDiscovery.class, args);
+    }
+}
