be4b0ccbfbbf28314908325bd9fb8a456300d144
Stanislav <mail@skov.dev>
2024-12-24 22:35:32 +0200

    [project-management-system#auth-service]: Adopted to RESTFul API principles

--- auth-service/src/main/java/local/pms/authservice/dto/SignInDto.java
+++ auth-service/src/main/java/local/pms/authservice/dto/SignInDto.java
@@ -1,3 +1,12 @@
 package local.pms.authservice.dto;
 
-public record SignInDto(String username, String password) {}
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotBlank;
+
+public record SignInDto(
+        @NotBlank(message = "Username is required")
+        @Size(min = 3, max = 255, message = "Username must be between 3 and 255 characters")
+        String username,
+        @NotBlank(message = "Password is required")
+        @Size(min = 3, max = 255, message = "Password must be between 3 and 255 characters")
+        String password) {}
--- auth-service/src/main/java/local/pms/authservice/dto/authuser/AuthUserDto.java
+++ auth-service/src/main/java/local/pms/authservice/dto/authuser/AuthUserDto.java
@@ -1,12 +1,23 @@
 package local.pms.authservice.dto.authuser;
 
-import java.util.List;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotBlank;
+
 import java.util.UUID;
+import java.util.List;
 
 public record AuthUserDto(
+        @NotBlank(message = "User identifier is required")
+        @Size(min = 36, max = 36, message = "User identifier must be 36 characters")
         UUID id,
+        @NotBlank(message = "Username is required")
+        @Size(min = 3, max = 255, message = "Username must be between 3 and 255 characters")
         String username,
+        @NotBlank(message = "Password is required")
+        @Size(min = 3, max = 255, message = "Password must be between 3 and 255 characters")
         String password,
+        @NotBlank(message = "Authentication roles is required")
         List<AuthRoleDto> authRoles,
+        @NotBlank(message = "Authentication permissions is required")
         List<AuthPermissionDto> authPermissions
 ) {}
