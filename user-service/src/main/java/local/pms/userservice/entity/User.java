b54635b927ce81df798b60202d13ead4856b1388
Stanislav <mail@skov.dev>
2025-07-11 23:02:21 +0300

    [project-management-system#user-service]: Added logical deleting

--- user-service/src/main/java/local/pms/userservice/entity/User.java
+++ user-service/src/main/java/local/pms/userservice/entity/User.java
@@ -1,32 +1,36 @@
 package local.pms.userservice.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Table;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Column;
 
 import lombok.Setter;
 import lombok.Getter;
-import lombok.AccessLevel;
 
-import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
-@Table(name = "project_management_system_user")
 @Setter
 @Getter
-@FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name = "project_management_system_user")
+@SQLRestriction(value = "deleted = false")
+@SQLDelete(sql = "UPDATE project_management_system_user SET deleted = true WHERE id = ?")
 public class User extends AbstractBaseModel {
 
     @Column(name = "first_name", nullable = false)
-    String firstName;
+    private String firstName;
 
     @Column(name = "last_name", nullable = false)
-    String lastName;
+    private String lastName;
 
     @Column(name = "email", nullable = false, unique = true)
-    String email;
+    private String email;
 
     @Column(name = "auth_user_id", nullable = false, unique = true)
-    String authUserId;
+    private String authUserId;
+
+    @Column(name = "deleted", nullable = false)
+    private boolean deleted = false;
 
 }
